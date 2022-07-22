998ffa6643025711bea520ce8186a87460a2981e
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-07-22 10:52:10 +0530

    changed Cors code

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ dotenv.config();
 //     })
 //   );
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://stack-over-flow-clone-backend.herokuapp.com");
+app.use(cors((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
-  });
+  }));
 
 app.use(express.json());
 
