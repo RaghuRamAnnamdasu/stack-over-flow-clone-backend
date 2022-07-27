71ffa3c3e3adead1eeb67f928400ef241da6e35a
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-07-27 14:19:16 +0530

    cors updation

--- index.js
+++ index.js
@@ -11,13 +11,24 @@ import nodemailer from "nodemailer";
 
 const app = express();
 
+dotenv.config();
 
-app.use(cors());
+
+app.use(cors((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+}
+
+));
 // dotenv.config();
 // app.use(express.json());
 
 
-dotenv.config(); 
+ 
 // app.use( cors({ origin: "*", }) );
 // app.use(
 //     cors({
@@ -25,14 +36,14 @@ dotenv.config();
 //     })
 //   );
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
 
 app.use(express.json());
 
